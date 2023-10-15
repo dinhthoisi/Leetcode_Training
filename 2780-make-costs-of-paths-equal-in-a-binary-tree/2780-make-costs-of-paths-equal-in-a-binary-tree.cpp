b232d69bc0d43e5121d96d3d113926bac37210d5
dinhthoisi <130883454+dinhthoisi@users.noreply.github.com>
2023-10-14 22:54:20 -0500

    Time: 168 ms (23.54%), Space: 140.8 MB (11.98%) - LeetHub

--- 2780-make-costs-of-paths-equal-in-a-binary-tree/2780-make-costs-of-paths-equal-in-a-binary-tree.cpp
+++ 2780-make-costs-of-paths-equal-in-a-binary-tree/2780-make-costs-of-paths-equal-in-a-binary-tree.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     int minIncrements(int n, vector<int>& cost) {
         int result = 0;
+        vector<int> dp(n,0);
         for(int i = n / 2 - 1; i >= 0; i--){
-            int left = i*2 + 1, right = i*2 + 2;
-            result += abs(cost[left]-cost[right]); // update result
-            cost[i] += max(cost[left],cost[right]); // update cost at node i
+            int left = i * 2 + 1;
+            int right = i * 2 + 2;
+            dp[i] = dp[left] + dp[right] + abs(cost[left] - cost[right]);
+            cost[i] += max(cost[left],cost[right]);
         }
-        return result;
+        return dp[0];
     }
 };
