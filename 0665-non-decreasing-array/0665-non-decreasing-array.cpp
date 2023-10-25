5d94b600ec54b5199566fdbd7c407008ac18d6bb
dinhthoisi <130883454+dinhthoisi@users.noreply.github.com>
2023-10-25 11:49:02 -0500

    Time: 26 ms (16.58%), Space: 27.4 MB (39.39%) - LeetHub

---
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int L = nums.size();
+        if(L == 1 || L == 2) return true; 
+        int count = 0;
+        int index = 0;
+        if(nums[0] > nums[1]){     
+            nums[0] = nums[1];
+            for(int i = 1; i < L - 1; i++){
+                if(nums[i] > nums[i+1])
+                    return false;
+            }
+        }
+
+        for(int i = 0; i < L - 1; i++){
+            if(i >= 1 && nums[i] > nums[i+1] && nums[i+1] >= nums[i-1]){
+                count++;
+                // modify nums[i], how about change nums[i+1] ?
+                nums[i] = nums[i+1];
+                index = i + 1;
+                break;         
+            }
+            else if(i >= 1 && nums[i] > nums[i+1] && nums[i+1] < nums[i-1]){
+                count++;
+                // change nums[i+1] ?
+                nums[i+1] = nums[i];
+                index = i + 1;
+                break;      
+            }
+        }
+        if(count == 1){
+            for(int i = index; i < L - 1; i++){
+                if(nums[i] > nums[i+1]) 
+                    return false;
+            }
+        }
+        return true;
+    }
+};
