c4db786cefbc6a89a40a1b2acefda60cc3abe062
dinhthoisi <130883454+dinhthoisi@users.noreply.github.com>
2023-10-26 13:04:41 -0500

    Time: 10 ms (67.38%), Space: 10.2 MB (7.25%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        int L = boxes.size();
-        vector<int> res(L,0);
-        for(int i = 0; i < L ; i++){
-            for(int j = 0; j < L; j++){
-                if(i!=j){
-                    if(boxes[j] == '1')
-                        res[i] += abs(i-j);
-                    if(boxes[i] == '1')
-                        res[j] += abs(i-j);
-                }
-            }
+        int N = boxes.size();
+        vector<int>leftcount(N,0);
+        vector<int>rightcount(N,0);
+        vector<int>leftcost(N,0);
+        vector<int>rightcost(N,0);
+        vector<int>tottalcost(N,0);
+
+        for(int i = 1; i < N; i++){
+            if(boxes[i-1] == '1') leftcount[i] = leftcount[i-1] + 1;
+            else leftcount[i] = leftcount[i-1];
+            leftcost[i] = leftcost[i-1] + leftcount[i];
         }
-        for(int i = 0; i < L; i++)
-            res[i] = res[i] / 2;
-        return res;
+        //for(int i = 1; i < N; i++){
+        //    leftcost[i] = leftcost[i-1] + leftcount[i];
+        //}
+        for(int i = N - 2; i >= 0; i--){
+            if(boxes[i+1] == '1') rightcount[i] = rightcount[i+1] + 1;
+            else rightcount[i] = rightcount[i+1];
+            rightcost[i] = rightcost[i+1] + rightcount[i];
+        }
+        //for(int i = N - 2; i >= 0; i--){
+        //    rightcost[i] = rightcost[i+1] + rightcount[i];
+        //}
+        for(int i = 0; i < N; i++){
+            tottalcost[i] = leftcost[i] + rightcost[i];
+        }
+
+        return tottalcost;
+
     }
 };
