f6d339b1552957879826d44656ee4be1349258bc
dinhthoisi <130883454+dinhthoisi@users.noreply.github.com>
2023-10-29 09:22:14 -0500

    Time: 258 ms (5.04%), Space: 95.4 MB (5.01%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int no_islands = 0;
+    vector<vector<vector<int>>> islands; // lưu hết các islands lại
     int numIslands(vector<vector<char>>& grid) {
         int L = grid.size();
         int H = grid[0].size();
@@ -10,19 +11,70 @@ public:
             for(int j = 0; j < H; j++){
                 if(visited[i][j] == 0 && grid[i][j] == '1'){
                     no_islands++;
-                    dfs(grid,i,j,visited);
+                    dfs(grid,i,j,visited,islands);
                 }
+            }
+        }
+
+        int no = islands.size();
+        for(int i = 0; i < no; i++){
+            cout << "island number " << no + 1 << endl; 
+            int len = islands[i].size();
+            for(int j = 0; j < len; j++){
+                cout << islands[i][j][0] << " " << islands[i][j][1] << endl;
             }
         }
 
         return no_islands;
     }
 
-    void dfs(vector<vector<char>>& grid, int i, int j, vector<vector<int>>& visited){
+    void dfs(vector<vector<char>>& grid, int i, int j, vector<vector<int>>& visited,vector<vector<vector<int>>>& islands){
         visited[i][j] = 1;
-        if(i >= 1 && visited[i-1][j] == 0 && grid[i-1][j] == '1') dfs(grid,i-1,j,visited);
-        if(j >= 1 && visited[i][j-1] == 0 && grid[i][j-1] == '1') dfs(grid,i,j-1,visited);
-        if(i+1 < grid.size() && visited[i+1][j] == 0 && grid[i+1][j] == '1') dfs(grid,i+1,j,visited);
-        if(j+1 < grid[0].size() && visited[i][j+1] == 0 && grid[i][j+1] == '1') dfs(grid,i,j+1,visited); 
+        if(i >= 1 && visited[i-1][j] == 0 && grid[i-1][j] == '1') {
+            vector<vector<int>> seperate_island;
+            vector<int> point;
+            point.push_back(i-1), point.push_back(j);
+            seperate_island.push_back(point);
+            islands.push_back(seperate_island);
+            dfs(grid,i-1,j,visited,islands);
+        }
+        if(j >= 1 && visited[i][j-1] == 0 && grid[i][j-1] == '1') {
+            vector<vector<int>> seperate_island;
+            vector<int> point;
+            point.push_back(i), point.push_back(j-1);
+            seperate_island.push_back(point);
+            islands.push_back(seperate_island);
+            dfs(grid,i,j-1,visited,islands);
+        }
+        if(i+1 < grid.size() && visited[i+1][j] == 0 && grid[i+1][j] == '1') {
+            vector<vector<int>> seperate_island;
+            vector<int> point;
+            point.push_back(i+1), point.push_back(j);
+            seperate_island.push_back(point);
+            islands.push_back(seperate_island);
+            dfs(grid,i+1,j,visited,islands);
+        }
+        if(j+1 < grid[0].size() && visited[i][j+1] == 0 && grid[i][j+1] == '1') {
+            vector<vector<int>> seperate_island;
+            vector<int> point;
+            point.push_back(i), point.push_back(j+1);
+            seperate_island.push_back(point);
+            islands.push_back(seperate_island);
+            dfs(grid,i,j+1,visited,islands); 
+        } 
     }
 };
+/*
+    làm sao biết được mỗi island sẽ có các ô nào ?
+    lưu các islands vào data structure vector<vector<vector<int>>> islands
+    như example 2, islands sẽ là :
+    islands[0] = [0,0] [0,1] [1,0] [1,1]
+        islands[0][0] = [0,0]
+            islands[0][0][0] = 0
+            islands[0][0][1] = 0
+        islands[0][1] = [0,1]
+        islands[0][2] = [1,0]
+        islands[0][3] = [1,1]            
+    islands[1] = [2,2] 
+    islands[2] = [3,3] [3,4] 
+*/
