2b4e7c53ff3c9e003f8e7ef199ac12f3956bef04
dinhthoisi <130883454+dinhthoisi@users.noreply.github.com>
2023-10-31 18:41:20 -0500

    Time: 144 ms (96.99%), Space: 110 MB (80.43%) - LeetHub

--- 2721-sum-of-distances/2721-sum-of-distances.cpp
+++ 2721-sum-of-distances/2721-sum-of-distances.cpp
@@ -3,39 +3,32 @@ public:
     vector<long long> distance(vector<int>& nums) 
     {
         int L = nums.size();
-        vector<long long>res(L);
+        vector<long long>res(L,0);
         unordered_map<int,vector<int>> m;
-
+        // build vector of indexes
         for (int i = 0; i < L; i++) 
             m[nums[i]].push_back(i);
-
+        // build res
         for (auto ite : m)
         {
             int num = ite.first;
-            vector<int>& indexes = ite.second;
-            //======================================================
+            vector<int>& indexes = ite.second; // sorted array
+            
             long long total_sum = 0;
             for (int index : indexes) 
                 total_sum += index;
             
-            long long pre_sum = 0;
+            long long left_sum = 0;
             int len = indexes.size();
             for (int i = 0; i < len; i++)
             {
                 int index = indexes[i];
-                long long post_sum = total_sum - pre_sum - index;
-                
-                res[index] += ((long long)index * i);
-                res[index] -= (pre_sum);
-                res[index] -= (index * (long long)(len - i - 1));
-                res[index] += post_sum;
-                
-                pre_sum += index;
+                long long right_sum = total_sum - left_sum - index;
+                res[index] = (long long)index * i - left_sum + right_sum - index * (long long)(len - i - 1);
+                left_sum += index;
             }
-            //======================================================
         }
-        return res;
-        
-        
+
+        return res;    
     }
 };
