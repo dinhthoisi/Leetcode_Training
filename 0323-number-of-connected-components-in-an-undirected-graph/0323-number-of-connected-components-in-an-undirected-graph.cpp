70718d6973f86f068c25ff3b07658a0390f12a5b
dinhthoisi <130883454+dinhthoisi@users.noreply.github.com>
2023-11-12 09:57:58 -0600

    Time: 64 ms (5.14%), Space: 26.4 MB (5.05%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int countComponents(int n, vector<vector<int>>& edges) {
+        // lưu các connected components vào gì đây ? 
+        map<int,set<int>> cc;
+
         // số lượng các cạnh trong đồ thị
         int L = edges.size();
         // danh sách kề : các đỉnh kề với a được lưu hết vào một set
@@ -15,49 +18,50 @@ public:
         // các đỉnh đã visited 
         set<int> visited;
         // duyệt qua các đỉnh trong danh sách kề
+        int cc_index = 0; // used for updated cc
         for(auto i: adj_list){
             count_not_isolated_nodes++;
             // nếu i.first chưa có trong visited thì thêm i vào trong visited 
-            // nếu i.first có trong visited rồi thì sao ?
+            // rồi dfs với các node trong danh sách kề của i.first
             if(visited.find(i.first) == visited.end()){
+                // mỗi lần tăng count lên là mình đã qua 1 cc mới 
+                set<int> new_cc;
                 count++;
                 visited.insert(i.first);
+                new_cc.insert(i.first);
                 for(auto j: i.second){
-                    dfs(visited,adj_list,j);
-                }    
+                    dfs(visited,adj_list,j,new_cc);
+                }
+                cc[cc_index] = new_cc; 
+                cc_index++;    
             }     
         }
 
+        // in cc ra kiểm tra coi code đúng ko 
+        
+        for(auto k: cc){
+            cout << k.first << ": "; 
+            for(auto h: k.second) 
+                cout << h << " ";
+            cout << endl;
+        }
+        
+
         return count + n - count_not_isolated_nodes;
     }
-    void dfs(set<int>& visited,map<int,set<int>>& adj_list, int j){
+    void dfs(set<int>& visited,map<int,set<int>>& adj_list, int j, set<int>& new_cc){
         if(visited.find(j) == visited.end()){
             visited.insert(j);
+            new_cc.insert(j);
             for(auto k: adj_list[j]){
-                dfs(visited,adj_list,k);
+                dfs(visited,adj_list,k, new_cc);
             }
         }    
     }
 };
 /*
-    làm sao để tìm số lượng các connected components 
-    sau đó lưu chúng lại ? 
-    ý tưởng là gì ? khi duyệt tới một node a mà a không có trong visited thì 
-    tăng count lên, sau đó update visited với các node trong danh sách kề của a
-    * này là bài toán dfs cơ bản mà ! hỏi Đô là bị la đấy 
-*/
-/*
-        int count_not_isolated_nodes = 0;
-        set<int> visited;
-        // số lượng các đỉnh trong graph có rồi 
-        for(auto i: adj_list){
-            count_not_isolated_nodes++;
-            if(visited.find(i.first) == visited.end()){
-                count++;
-                visited.insert(i.first);
-            }
-            for(auto j: i.second)
-                visited.insert(j);
-        }
-        return count + (n - count_not_isolated_nodes);
+    okie, viết xong dfs rồi thì tìm cách lưu các connected components lại 
+    vd đầu tiên thì mình có 2 cc là 0, 1, 2 và 3, 4 
+    vd thứ hai thì mình chỉ có 1 cc là 0, 1, 2, 3, 4 
+    lưu mỗi cc vào một cái set có vẻ ổn 
 */
