0a027d31bbe9adc6aaae03770c45e5bcd42279cd
dinhthoisi <130883454+dinhthoisi@users.noreply.github.com>
2023-11-13 15:53:17 -0600

    Time: 19 ms (21.99%), Space: 6.6 MB (87.43%) - LeetHub

--- 2786-find-the-longest-semi-repetitive-substring/2786-find-the-longest-semi-repetitive-substring.cpp
+++ 2786-find-the-longest-semi-repetitive-substring/2786-find-the-longest-semi-repetitive-substring.cpp
@@ -1,34 +1,17 @@
 class Solution {
 public:
     int longestSemiRepetitiveSubstring(string s) {
-        int res = 2;
-        int L = s.size();
-        if(L==1) return 1;
-
-        for(int i = 0; i < L - 1; i++){
-            for(int j = i + 2; j < L; j++){
-                if(semiRepetitiveSubstringChecking(s.substr(i, j-i+1)) == true)
-                    res = max(res,j-i+1);
+        int res = 1, left = 0, right = 1, last = 0;
+        while(right < s.size()){
+            if(s[right] == s[right-1]){
+                // this is needed only for the 1st duplicate found, as we don't want to update the value
+                // of left for the 1st duplicate. As one duplicate is allowed. 
+                if(last) left = last;
+                last = right;
             }
+            res = max(res,right-left+1);
+            right++;
         }
-
         return res;
     }
-
-    bool semiRepetitiveSubstringChecking(string s){
-        int count = 0;
-        int L = s.size();
-        for(int i = 0; i < L - 1; i++){
-            if(s[i] == s[i+1]){
-                count++;
-            }
-        }
-        if(count <= 1) return true;
-        return false;
-    }
 };
-/*
-    2 con trỏ di chuyển như thế nào đây ??? 
-
-    
-*/
